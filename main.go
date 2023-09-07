a6e8077acbef467cf725c869f05944c6f513466f
mangushev-ildar <mangushev.ildar@bk.ru>
2023-09-07 23:38:02 +0300

    add button

--- main.go
+++ main.go
@@ -7,6 +7,21 @@ import (
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
+//кнопки
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+    tgbotapi.NewKeyboardButtonRow(
+        tgbotapi.NewKeyboardButton("1"),
+        tgbotapi.NewKeyboardButton("2"),
+        tgbotapi.NewKeyboardButton("3"),
+    ),
+    tgbotapi.NewKeyboardButtonRow(
+        tgbotapi.NewKeyboardButton("4"),
+        tgbotapi.NewKeyboardButton("5"),
+        tgbotapi.NewKeyboardButton("6"),
+    ),
+)
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI("5337023432:AAGWQ7HDN8mLTYGkeg8WbEdGefCnRuylHSw")
 	if err != nil {
@@ -48,6 +63,14 @@ func main() {
 			log.Printf("[%s]/n %s/n", update.Message.From.UserName, update.Message.Text)
 			msg.ReplyToMessageID = update.Message.MessageID
 		}
+
+		//открытие кнопок
+		switch update.Message.Text {
+        case "open":
+            msg.ReplyMarkup = numericKeyboard
+        case "close":
+            msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+        }
 
         if _, err := bot.Send(msg); err != nil {
             log.Panic(err)
